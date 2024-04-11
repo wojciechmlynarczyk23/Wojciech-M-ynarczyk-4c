128e9e9c285a0250eeffef9c06b120ecf6f7646d
wojciechmlynarczyk23 <145118821+wojciechmlynarczyk23@users.noreply.github.com>
2024-04-11 08:36:02 +0200

    Add files via upload

---
+++ 11.04/script.js
@@ -0,0 +1,34 @@
+function createUser(e) {  
+    e.preventDefault() 
+    const name = document.querySelector('#name').value; 
+    const surname = document.querySelector('#name').value; 
+    const description = document.querySelector('#name').value; 
+
+    if(name !='' && surname !='' && description!= ''){ 
+        insertRow(name,surname,description)
+    } 
+    else{ 
+        alert("All fields are required")
+    }
+}  
+function insertRow(name,surname,description){ 
+    const row = document.createElement('tr'); 
+    const nameCell = document.createElement('td'); 
+    nameCell.textContent = name; 
+    row.appendChild(nameCell); 
+
+    const surnameCell = document.createElement('td'); 
+    surnameCell.textContent = surname; 
+    row.appendChild(surnameCell); 
+
+    const descriptionCell = document.createElement('td'); 
+    descriptionCell.textContent = description; 
+    row.appendChild(descriptionCell); 
+
+    const table = document.querySelector("#users-holder"); 
+    table.appendChild(row);
+}
+(() =>{ 
+    const form = document.querySelector("#users-form"); 
+    form.addEventListener('submit',createUser); 
+})()
